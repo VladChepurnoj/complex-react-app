ac1321538c507fd3d24ad417fb801108e08768d6
Vlad <chepurnojv@gmail.com>
2021-07-15 16:20:06 +0300

    added flash mesages, implemented create post screen

--- app/Main.js
+++ app/Main.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Axios from "axios";
+Axios.defaults.baseURL = "http://localhost:8080";
 
 //compon
 import Header from "./components/Header";
@@ -9,14 +11,23 @@ import Home from "./components/Home";
 import Footer from "./components/Footer";
 import About from "./components/About";
 import Terms from "./components/Terms";
+import CreatePost from "./components/CreatePost";
+import ViewSinglePost from "./components/ViewSinglePost";
+import FlashMessages from "./components/FlashMessages";
 
 function Main() {
   const [loggedIn, setLoggedIn] = useState(
     Boolean(localStorage.getItem("complexappToken"))
   );
+  const [flashMessages, setFlashMessages] = useState([]);
+
+  function addFlashMessage(msg) {
+    setFlashMessages((prev) => prev.concat(msg));
+  }
 
   return (
     <BrowserRouter>
+      <FlashMessages messages={flashMessages} />
       <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
 
       <Switch>
@@ -26,6 +37,14 @@ function Main() {
 
         <Route path='/about-us'>
           <About />
+        </Route>
+
+        <Route path='/post/:id'>
+          <ViewSinglePost />
+        </Route>
+
+        <Route path='/create-post'>
+          <CreatePost addFlashMessage={addFlashMessage} />
         </Route>
 
         <Route path='/terms'>
