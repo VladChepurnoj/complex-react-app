99326646898f7b829308d2d46db971625b71dc75
Vlad <chepurnojv@gmail.com>
2021-07-19 21:47:01 +0300

    profile screen with real posts added

--- app/Main.js
+++ app/Main.js
@@ -18,6 +18,7 @@ import Terms from "./components/Terms";
 import CreatePost from "./components/CreatePost";
 import ViewSinglePost from "./components/ViewSinglePost";
 import FlashMessages from "./components/FlashMessages";
+import Profile from "./components/Profile";
 
 function Main() {
   const initialState = {
@@ -68,6 +69,10 @@ function Main() {
           <Header />
 
           <Switch>
+            <Route path='/profile/:username'>
+              <Profile />
+            </Route>
+
             <Route path='/' exact>
               {state.loggedIn ? <Home /> : <HomeGuest />}
             </Route>
