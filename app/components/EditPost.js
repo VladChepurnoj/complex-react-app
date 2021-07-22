524f2ffba3aff7c11909ae2448dd45952bd9f184
Vlad <chepurnojv@gmail.com>
2021-07-22 19:39:19 +0300

    edit post func.updated

--- app/components/EditPost.js
+++ app/components/EditPost.js
@@ -1,11 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useImmerReducer } from "use-immer";
 import Page from "./Page";
 import Axios from "axios";
 import { useParams, Link } from "react-router-dom";
 import LoadingDotsIcon from "./LoadingDotsIcon";
+import StateContext from "../StateContext";
+import DispatchContext from "../DispatchContext";
 
 const EditPost = () => {
+  const appState = useContext(StateContext);
+  const appDispatch = useContext(DispatchContext);
+
   const originalState = {
     title: {
       value: "",
@@ -30,14 +35,30 @@ const EditPost = () => {
         draft.body.value = action.value.body;
         draft.isFetching = false;
         return;
+      case "titleChange":
+        draft.title.value = action.value;
+        return;
+      case "bodyChange":
+        draft.body.value = action.value;
+        return;
+      case "submitRequest":
+        draft.sendCount++;
+        return;
+      case "saveRequestStarted":
+        draft.isSaving = true;
+        return;
+      case "saveRequestFinished":
+        draft.isSaving = false;
+        return;
     }
   }
 
   const [state, dispatch] = useImmerReducer(ourReducer, originalState);
 
-  // const { id } = useParams();
-  // const [isLoading, setIsLoading] = useState(true);
-  // const [post, setPost] = useState();
+  function submitHandler(e) {
+    e.preventDefault();
+    dispatch({ type: "submitRequest" });
+  }
 
   useEffect(() => {
     const ourRequest = Axios.CancelToken.source();
@@ -57,6 +78,37 @@ const EditPost = () => {
     };
   }, []);
 
+  useEffect(() => {
+    if (state.sendCount) {
+      dispatch({ type: "saveRequestStarted" });
+      const ourRequest = Axios.CancelToken.source();
+      async function fetchPost() {
+        try {
+          const response = await Axios.post(
+            `/post/${state.id}/edit`,
+            {
+              title: state.title.value,
+              body: state.body.value,
+              token: appState.user.token,
+            },
+            {
+              cancelToken: ourRequest.token,
+            }
+          );
+
+          dispatch({ type: "saveRequestFinished" });
+          appDispatch({ type: "flashMessage", value: "Post was updated" });
+        } catch (err) {
+          console.log("there was a problem or the request was cancelled");
+        }
+      }
+      fetchPost();
+      return () => {
+        ourRequest.cancel();
+      };
+    }
+  }, [state.sendCount]);
+
   if (state.isFetching)
     return (
       <Page title='...'>
@@ -66,12 +118,15 @@ const EditPost = () => {
 
   return (
     <Page title='Edit post'>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className='form-group'>
           <label htmlFor='post-title' className='text-muted mb-1'>
             <small>Title</small>
           </label>
           <input
+            onChange={(e) =>
+              dispatch({ type: "titleChange", value: e.target.value })
+            }
             value={state.title.value}
             autoFocus
             name='title'
@@ -88,6 +143,9 @@ const EditPost = () => {
             <small>Body Content</small>
           </label>
           <textarea
+            onChange={(e) =>
+              dispatch({ type: "bodyChange", value: e.target.value })
+            }
             name='body'
             id='post-body'
             className='body-content tall-textarea form-control'
@@ -96,7 +154,9 @@ const EditPost = () => {
           />
         </div>
 
-        <button className='btn btn-primary'>Save Updates</button>
+        <button className='btn btn-primary' disabled={state.isSaving}>
+          Save Updates
+        </button>
       </form>
     </Page>
   );
