7b521654b0f281a12d00494035a78b54303db2d5
Vlad <chepurnojv@gmail.com>
2021-07-26 12:26:03 +0300

    search feature, fetching posts from database

--- app/components/Search.js
+++ app/components/Search.js
@@ -1,18 +1,77 @@
 import React, { useContext, useEffect } from "react";
 import DispatchContext from "../DispatchContext";
+import { useImmer } from "use-immer";
+import Axios from "axios";
 
 const Search = () => {
   const appDispatch = useContext(DispatchContext);
+
+  const [state, setState] = useImmer({
+    searchTerm: "",
+    results: [],
+    show: "neither",
+    requestCount: 0,
+  });
 
   useEffect(() => {
     document.addEventListener("keyup", searchKeyPressHandler);
     return () => document.removeEventListener("keyup", searchKeyPressHandler);
   }, []);
 
+  useEffect(() => {
+    if (state.searchTerm.trim()) {
+      setState((draft) => {
+        draft.show = "loading";
+      });
+      const delay = setTimeout(() => {
+        setState((draft) => {
+          draft.requestCount++;
+        });
+      }, 2000);
+
+      return () => clearTimeout(delay);
+    } else {
+      setState((draft) => {
+        draft.show = "neither";
+      });
+    }
+  }, [state.searchTerm]);
+
+  useEffect(() => {
+    if (state.requestCount) {
+      const ourRequest = Axios.CancelToken.source();
+      async function fetchResults() {
+        try {
+          const response = await Axios.post(
+            "/search",
+            { searchTerm: state.searchTerm },
+            { cancelToken: ourRequest.token }
+          );
+
+          setState((draft) => {
+            draft.result = response.data;
+            draft.show = "results";
+          });
+        } catch (error) {
+          console.log("there was a problem");
+        }
+      }
+      fetchResults();
+      return () => ourRequest.cancel();
+    }
+  }, [state.requestCount]);
+
   function searchKeyPressHandler(e) {
     if (e.keyCode == 27) {
       appDispatch({ type: "closeSearch" });
     }
+  }
+
+  function handleInput(e) {
+    const value = e.target.value;
+    setState((draft) => {
+      draft.searchTerm = value;
+    });
   }
 
   return (
@@ -24,6 +83,7 @@ const Search = () => {
               <i className='fas fa-search'></i>
             </label>
             <input
+              onChange={handleInput}
               autoFocus
               type='text'
               autoComplete='off'
@@ -42,7 +102,18 @@ const Search = () => {
 
         <div className='search-overlay-bottom'>
           <div className='container container--narrow py-3'>
-            <div className='live-search-results live-search-results--visible'>
+            <div
+              className={
+                "circle-loader " +
+                (state.show == "loading" ? "circle-loader--visible" : "")
+              }
+            ></div>
+            <div
+              className={
+                "live-search-results " +
+                (state.show == "results" ? "live-search-results--visible" : "")
+              }
+            >
               <div className='list-group shadow-sm'>
                 <div className='list-group-item active'>
                   <strong>Search Results</strong> (3 items found)
