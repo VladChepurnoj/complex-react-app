8f7fca97185b7656b5d7afa183231f819eeff708
Vlad <chepurnojv@gmail.com>
2021-07-31 17:44:17 +0300

    chat feature started to do

--- app/Main.js
+++ app/Main.js
@@ -23,6 +23,7 @@ import Profile from "./components/Profile";
 import EditPost from "./components/EditPost";
 import NotFound from "./components/NotFound";
 import Search from "./components/Search";
+import Chat from "./components/Chat";
 
 function Main() {
   const initialState = {
@@ -34,6 +35,7 @@ function Main() {
       avatar: localStorage.getItem("complexappAvatar"),
     },
     isSearchOpen: false,
+    isChatOpen: false,
   };
 
   function ourReducer(draft, action) {
@@ -56,6 +58,12 @@ function Main() {
 
       case "closeSearch":
         draft.isSearchOpen = false;
+        return;
+      case "toggleChat":
+        draft.isChatOpen = !draft.isChatOpen;
+        return;
+      case "closeChat":
+        draft.isChatOpen = false;
         return;
     }
   }
@@ -122,6 +130,7 @@ function Main() {
           >
             <Search />
           </CSSTransition>
+          <Chat />
           <Footer />
         </BrowserRouter>
       </DispatchContext.Provider>
--- app/components/HeaderLoggedIn.js
+++ app/components/HeaderLoggedIn.js
@@ -30,6 +30,7 @@ const HeaderLoggedIn = (props) => {
       </a>
       <ReactTooltip place='bottom' id='search' className='custom-tooltip' />{" "}
       <span
+        onClick={() => appDispatch({ type: "toggleChat" })}
         data-for='chat'
         data-tip='Chat'
         className='mr-2 header-chat-icon text-white'
